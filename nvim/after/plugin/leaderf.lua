a4b095e75eb5224da52dede4bd08946e5388fd2a
Tianxi Ku <kutianxi@outlook.com>
2022-09-14 12:21:50 +0800

    update leaderf plugin

--- nvim/after/plugin/leaderf.lua
+++ nvim/after/plugin/leaderf.lua
@@ -1,5 +1,5 @@
 vim.cmd [[
   nnoremap <silent> <leader>ff :<C-U>Leaderf file<CR>
-  nnoremap <silent> <leader>rg :<C-U>Leaderf rg 
+  nnoremap <silent> <leader>rg :<C-U>Leaderf rg<CR>
   noremap <leader>gg :<C-U>Leaderf! rg --recall<CR>
 ]]
