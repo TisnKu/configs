d2c9f0884b0f2a7d545bd733183a49adaf969cf1
Tianxi Ku <kutianxi@outlook.com>
2022-09-14 22:56:44 +0800

    use enter to confirm selection

--- nvim/after/plugin/leaderf.lua
+++ nvim/after/plugin/leaderf.lua
@@ -1,4 +1,5 @@
 vim.cmd [[
+  nnoremap <silent> <Leader>f<space> :Leaderf 
   nnoremap <silent> <leader>ff :<C-U>Leaderf file<CR>
   nnoremap <silent> <leader>rg :<C-U>Leaderf rg<CR>
   noremap <leader>gg :<C-U>Leaderf! rg --recall<CR>
