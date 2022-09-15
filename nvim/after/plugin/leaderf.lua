18c629b51fac288055841ef94c39e05e5096d6c5
Tianxi Ku <kutianxi@outlook.com>
2022-09-15 13:43:23 +0800

    update leaderf config

--- nvim/after/plugin/leaderf.lua
+++ nvim/after/plugin/leaderf.lua
@@ -1,6 +1,13 @@
 vim.cmd [[
+  let g:Lf_UseCache=1
+  let g:Lf_RgConfig = [
+    \ "--max-columns=150",
+    \ "--glob=!node_modules/*",
+    \ "--glob=!dist/*",
+    \ "--glob=!*/*patch",
+    \ ]
   nnoremap <silent> <Leader>f<space> :Leaderf 
   nnoremap <silent> <leader>ff :<C-U>Leaderf file<CR>
-  nnoremap <silent> <leader>rg :<C-U>Leaderf rg<CR>
+  nnoremap <silent> <leader>rg :<C-U>Leaderf rg -i<CR>
   noremap <leader>gg :<C-U>Leaderf! rg --recall<CR>
 ]]
