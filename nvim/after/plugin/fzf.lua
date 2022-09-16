b7b1221351fd52cbc0abe099f85ba7f16a1570ba
Tianxi Ku <kutianxi@outlook.com>
2022-09-16 14:39:02 +0800

    polish fzf

--- nvim/after/plugin/fzf.lua
+++ nvim/after/plugin/fzf.lua
@@ -1,8 +1,11 @@
-vim.keymap.set("n", "<leader>f", "<cmd>call fzf#run(fzf#wrap({\"source\": \"rg --files --hidden\"}))<cr>")
+vim.keymap.set("n", "<leader>f", "<cmd>call fzf#run(fzf#wrap({\"source\": \"rg --files\"}))<cr>")
+--vim.keymap.set("n", "<leader>f", ":FZF<cr>")
 vim.keymap.set("n", "<leader>rg", ":RG<cr>")
 vim.keymap.set("n", "<leader>zg", ":Rg<cr>")
 
 vim.cmd [[
+  let g:fzf_layout = { 'window': { 'width': 1, 'height': 0.4, 'yoffset': 1, 'border': 'horizontal' } }
+
   function! RipgrepFzf(query, fullscreen)
     let command_fmt = 'rg --column --line-number --no-heading --color=always --smart-case -- %s || true'
     let initial_command = printf(command_fmt, shellescape(a:query))
