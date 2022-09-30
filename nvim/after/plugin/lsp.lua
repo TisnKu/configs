64616c73e2b3b045d04eb79047c5940a3516399a
Tianxi Ku <kutianxi@outlook.com>
2022-09-30 12:21:08 +0800

    setup powershell lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -33,8 +33,10 @@ require('lsp-setup').setup({
 	servers = {
 		-- Install LSP servers automatically
 		-- LSP server configuration please see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-		-- pylsp = {},
 		['null-ls'] = {},
+		['powershell_es'] = {
+			bundle_path = vim.fn.stdpath('data') .. '\\mason\\packages\\powershell-editor-services',
+		},
 		taplo = {},
 		tsserver = {},
 		sumneko_lua = {
