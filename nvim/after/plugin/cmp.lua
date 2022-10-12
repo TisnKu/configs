db0b6938f9a50edcdd97bdad896d4683ca4780d4
Tianxi Ku <kutianxi@outlook.com>
2022-10-12 17:48:13 +0800

    Revert "fix ctrl+n not insert issue"
    This reverts commit c93f1329f2b22438a32069719c5ced4cec3df4ac.

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -1,45 +1,43 @@
 local cmp = require "cmp"
 cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-    end
-  },
-  --comfirmation = { completeopt = "menu,menuone,noselect" },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })),
-    ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })),
-    ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })),
-    ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })),
-    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-    ["<Enter>"] = cmp.mapping.confirm({ select = false }),
-  }),
-  preselect = cmp.PreselectMode.None,
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "vsnip" },
-    { name = "buffer" }
-  }),
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        end
+    },
+    --comfirmation = { completeopt = "menu,menuone,noselect" },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })),
+        ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<Enter>"] = cmp.mapping.confirm({ select = false }),
+    }),
+    preselect = cmp.PreselectMode.None,
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "vsnip" },
+        { name = "buffer" }
+    }),
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won"t work anymore).
 cmp.setup.cmdline("/", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = { {
-    name = "buffer"
-  } }
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = { {
+        name = "buffer"
+    } }
 })
 
 -- Use cmdline & path source for ":" (if you enabled `native_menu`, this won"t work anymore).
 cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({ {
-    name = "path"
-  } }, { {
-    name = "cmdline"
-  } })
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({ {
+        name = "path"
+    } }, { {
+        name = "cmdline"
+    } })
 })
