54e4d8e47afe66215a0d1dc64f6e390ba83a0fae
Tianxi Ku <kutianxi@outlook.com>
2022-10-14 10:02:17 +0800

    add resume keymap

--- nvim/after/plugin/fzflua.lua
+++ nvim/after/plugin/fzflua.lua
@@ -1,20 +1,20 @@
 local status, FzfLua = pcall(require, "fzf-lua")
 if not status then
-    return
+  return
 end
 
 require('fzf-lua').setup {
-    winopts = {
-        win_height = 0.7,
-        win_width = 0.9,
-        win_row = 0.5,
-        win_col = 0.5,
-    },
-    --fzf_layout = 'reverse-list',
-    fzf_opts = {
-        ['--info'] = 'default',
-        ['--keep-right'] = '',
-    }
+  winopts = {
+    win_height = 0.7,
+    win_width = 0.9,
+    win_row = 0.5,
+    win_col = 0.5,
+  },
+  --fzf_layout = 'reverse-list',
+  fzf_opts = {
+    ['--info'] = 'default',
+    ['--keep-right'] = '',
+  }
 }
 
 vim.cmd("cnoreabbrev FL FzfLua")
@@ -31,3 +31,4 @@ vim.keymap.set('n', 'gi', ':lua require("fzf-lua").lsp_implementations({ jump_to
 vim.keymap.set('n', 'gr', ':lua require("fzf-lua").lsp_references({ jump_to_single_result = true })<CR>', opts)
 vim.keymap.set('n', '<leader>m', ':lua require("fzf-lua").keymaps()<CR>', opts)
 vim.keymap.set('n', '<leader>b', ':lua require("fzf-lua").buffers()<CR>', opts)
+vim.keymap.set('n', '<leader>rs', ':lua require("fzf-lua").resume()<CR>', opts)
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,25 +1,25 @@
 local ok, telescope = pcall(require, "telescope")
 if not ok then
-    return
+  return
 end
 
 local actions = require("telescope.actions")
 telescope.setup {
-    defaults = {
-        file_ignore_patterns = { "node_modules" },
-        preview = {
-            treesitter = false,
-        },
-        dynamic_preview_title = true,
-        path_display = { "truncate" },
-        mappings = {
-            i = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<esc>"] = actions.close,
-            },
-        },
+  defaults = {
+    file_ignore_patterns = { "node_modules" },
+    preview = {
+      treesitter = false,
     },
+    dynamic_preview_title = true,
+    path_display = { "truncate" },
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<esc>"] = actions.close,
+      },
+    },
+  },
 }
 telescope.load_extension('fzy_native')
 local opts = { noremap = true, silent = true }
@@ -31,6 +31,7 @@ vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
 vim.keymap.set("n", "<leader>m", "<cmd>Telescope keymaps<CR>", opts)
 vim.keymap.set("n", "<leader>gst", "<cmd>Telescope git_status<CR>", opts)
+vim.keymap.set("n", "<leader>rs", "<cmd>Telescope resume<CR>", opts)
 
 vim.cmd [[
   autocmd User TelescopePreviewerLoaded setlocal wrap
