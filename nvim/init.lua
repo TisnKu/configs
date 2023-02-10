3b773c88dfbd0b116f649b88467961678fbee8b3
Tianxi Ku <kutianxi@outlook.com>
2023-02-10 17:15:03 +0800

    use lazy nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,91 +18,78 @@ vim.g.is_linux = (has("unix") and (not has("macunix"))) and true or false
 vim.g.is_mac = has("macunix") and true or false
 
 -- Plugins
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
-local packer_bootstrap = ensure_packer()
+vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
-require("packer").startup(function(use)
-  local optuse = function(plugin, opts)
-    if opts == nil then
-      opts = { opt = true }
-    else
-      opts.opt = true
-    end
+require("lazy").setup({ spec = {
+  { "projekt0n/github-nvim-theme", lazy = true },
+  { "kaicataldo/material.vim", branch = "main" },
 
-    use(plugin, opts)
-  end
-
-  optuse("wbthomason/packer.nvim")
-  optuse("projekt0n/github-nvim-theme")
-  optuse("kaicataldo/material.vim", { branch = "main" })
-
-  optuse("machakann/vim-sandwich")
-  optuse("preservim/nerdtree")
-  optuse("scrooloose/nerdcommenter")
-  optuse("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-  optuse("windwp/nvim-autopairs")
-  optuse("yuttie/comfortable-motion.vim") -- Smooth scrolling
-  optuse({
+  { "machakann/vim-sandwich", lazy = true },
+  "preservim/nerdtree",
+  { "scrooloose/nerdcommenter", lazy = true },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", lazy = true },
+  "windwp/nvim-autopairs",
+  "yuttie/comfortable-motion.vim", -- Smooth scrolling
+  {
     "lukas-reineke/indent-blankline.nvim", -- Indentation lines
-    requires = { { "nvim-treesitter/nvim-treesitter", opt = true } },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
       vim.opt.list = true
       require("indent_blankline").setup({
         show_current_context = true,
       })
     end,
-  })
+  },
 
-  optuse("dstein64/vim-startuptime")
-  optuse("github/copilot.vim")
-  optuse("nvim-lualine/lualine.nvim")
-  optuse("nvim-lua/plenary.nvim")
-  optuse("lewis6991/gitsigns.nvim")
-  optuse { 'sindrets/diffview.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  "dstein64/vim-startuptime",
+  "github/copilot.vim",
+  "nvim-lualine/lualine.nvim",
+  "nvim-lua/plenary.nvim",
+  "lewis6991/gitsigns.nvim",
+  { 'sindrets/diffview.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
 
-  optuse({ "junegunn/fzf", run = ":call fzf#install()" })
-  if vim.g.is_win then
-    optuse { "nvim-telescope/telescope.nvim", tag = "0.1.0",
-      requires = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzy-native.nvim" } }
-  else
-    optuse({ "ibhagwan/fzf-lua", requires = { { "kyazdani42/nvim-web-devicons", opt = true } } })
-  end
-  optuse({ "hood/popui.nvim", requires = { 'RishabhRD/popfix' } })
+  { "junegunn/fzf", build = ":call fzf#install()" },
+  --if vim.g.is_win then
+  --  optuse { "nvim-telescope/telescope.nvim", tag = "0.1.0",
+  --    requires = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzy-native.nvim" } }
+  --else
+  { "ibhagwan/fzf-lua", dependencies = { "kyazdani42/nvim-web-devicons" } },
+  --end
+  { "hood/popui.nvim", dependencies = { 'RishabhRD/popfix' } },
 
-  optuse("williamboman/mason.nvim")
-  optuse("jose-elias-alvarez/null-ls.nvim")
-  optuse("williamboman/mason-lspconfig.nvim")
-  optuse("neovim/nvim-lspconfig")
-  optuse({
+  "williamboman/mason.nvim",
+  "jose-elias-alvarez/null-ls.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  {
     "TisnKu/lsp-setup.nvim",
-    requires = {
-      { "neovim/nvim-lspconfig", opt = true },
-      { "williamboman/mason.nvim", opt = true },
-      { "williamboman/mason-lspconfig.nvim", opt = true },
+    dependencies = {
+      { "neovim/nvim-lspconfig", lazy = true },
+      { "williamboman/mason.nvim", lazy = true },
+      { "williamboman/mason-lspconfig.nvim", lazy = true },
     },
-  })
+  },
 
-  optuse("hrsh7th/cmp-nvim-lsp")
-  optuse("hrsh7th/cmp-buffer")
-  optuse("hrsh7th/cmp-path")
-  optuse("hrsh7th/cmp-cmdline")
-  optuse("hrsh7th/cmp-vsnip")
-  optuse("hrsh7th/vim-vsnip")
-  optuse("hrsh7th/nvim-cmp")
-
-  if packer_bootstrap then
-    require("packer").sync()
-  end
-end)
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/cmp-vsnip",
+  "hrsh7th/vim-vsnip",
+  "hrsh7th/nvim-cmp",
+}, defaults = {
+  lazy = false,
+} })
 -- End plugins
