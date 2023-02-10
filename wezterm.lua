7f098905425b18e22b236859b9b3eccd8a853aa9
Tianxi Ku <kutianxi@outlook.com>
2023-02-10 17:38:47 +0800

    update wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,25 @@
 local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
 
 return {
   font = wezterm.font 'JetBrains Mono',
-  default_prog = { 'pwsh.exe' },
+  default_domain = 'WSL:Ubuntu',
+  --default_prog = { 'pwsh.exe' },
   keys = {
     {
       key = 'z', mods = 'ALT', action = wezterm.action.ShowLauncher,
     }
-  }
+  },
+  launch_menu = {
+    {
+      label = "Powershell",
+      args = { 'pwsh.exe' }
+    }
+  },
+  window_decorations = "NONE"
 }
