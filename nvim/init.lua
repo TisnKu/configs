3d792b658736da6d5e4d3cad24927057e535848a
Tianxi Ku <kutianxi@outlook.com>
2023-10-14 02:44:21 +0000

    fix configs on ubuntu

--- nvim/init.lua
+++ nvim/init.lua
@@ -122,9 +122,7 @@ require("packer").startup(function(use)
     requires = { { "nvim-treesitter/nvim-treesitter", opt = true } },
     config = function()
       vim.opt.list = true
-      require("indent_blankline").setup({
-        show_current_context = true,
-      })
+      require("ibl").setup({})
     end,
   })
 
