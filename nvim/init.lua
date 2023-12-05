e6b19d28a788196d7551ba4b314f8586b2afc86e
Tianxi Ku <kutianxi@outlook.com>
2023-12-05 17:49:16 +0800

    set default shell to pwsh in windows

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,6 +42,16 @@ if vim.g.is_wsl then
     cache_enabled = 0,
   }
 end
+if vim.g.is_win then
+  vim.cmd [[
+		let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+		let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;'
+		let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+		let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+		set shellquote= shellxquote=
+  ]]
+end
+
 require("completeCurrentLine")
 
 -- Plugins
