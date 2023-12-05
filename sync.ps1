b872ad506eae692d8f6c5b914fefc4d54602a7f0
Tianxi Ku <kutianxi@outlook.como>
2023-12-05 16:32:41 +0800

    update sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -1,4 +1,4 @@
 New-Item -Path $env:USERPROFILE\.vimrc -ItemType SymbolicLink -Value $env:USERPROFILE\configs\nvim\vimrc
 New-Item -Path $env:USERPROFILE\AppData\Local\nvim -ItemType SymbolicLink -Value $env:USERPROFILE\configs\nvim\
 New-Item -Path $env:USERPROFILE\.wezterm.lua -ItemType SymbolicLink -Value $env:USERPROFILE\configs\wezterm.lua
-New-Item -Path "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\appLaunchers.ahk" -ItemType HardLink -Value $env:USERPROFILE\Scripts\ahk\appLaunchers.ahk
+
