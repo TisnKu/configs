b4b6722f8e016e2e6bc9ddcf0df6696e128d3e6a
Tianxi Ku <tiaku@microsoft.com>
2023-12-14 14:00:11 +0800

    maximize wezterm on startup

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,10 @@
 local wezterm = require 'wezterm'
 
+wezterm.on("gui-startup", function()
+  local _, _, window = wezterm.mux.spawn_window {}
+  window:gui_window():maximize()
+end)
+
 local schemes = {}
 for name, _ in pairs(wezterm.get_builtin_color_schemes()) do
   schemes[#schemes + 1] = {
