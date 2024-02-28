b272235dc7e7d6b3c3ebb582a30de5b91e6f697b
Tianxi Ku <kutianxi@outlook.com>
2024-02-28 14:23:34 +0800

    fix powershell nerdtree command

--- nvim/lua/configs/ps.lua
+++ nvim/lua/configs/ps.lua
@@ -5,5 +5,8 @@ if vim.g.is_win then
 		let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
 		let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
 		set shellquote= shellxquote=
-  ]]
+    let g:NERDTreeCopyFileCmd = 'cp -r ' "https://github.com/preservim/nerdtree/blob/f3a4d8eaa8ac10305e3d53851c976756ea9dc8e8/plugin/NERD_tree.vim#L96
+    let g:NERDTreeRemoveDirCmd = 'rm -r '
+    let g:NERDTreeCopyDirCmd = 'cp -r '
+]]
 end
