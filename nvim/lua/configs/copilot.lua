b31cef3bf54eddba598a503d1c63cd9a141a6b12
Tianxi Ku <kutianxi@outlook.como>
2024-03-30 09:39:16 +0800

    add copilot config

---
+++ nvim/lua/configs/copilot.lua
@@ -0,0 +1,16 @@
+require('utils').trySetup("CopilotChat", {
+  debug = true
+})
+
+-- Keybindings
+-- github copilot keys
+if vim.g.is_mac then
+  vim.api.nvim_set_keymap('i', '¬', '<Plug>(copilot-next)', {})
+  vim.api.nvim_set_keymap('i', '˙', '<Plug>(copilot-previous)', {})
+else
+  vim.api.nvim_set_keymap('i', '<A-l>', '<Plug>(copilot-next)', {})
+  vim.api.nvim_set_keymap('i', '<A-h>', '<Plug>(copilot-previous)', {})
+end
+
+-- Copilot chat
+vim.api.nvim_set_keymap('n', '<space>cc', '<ESC>:CopilotChatToggle<CR>', { noremap = true, silent = true })
