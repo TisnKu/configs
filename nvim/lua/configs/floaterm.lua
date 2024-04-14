e6e50d8b4e5b55f8dc1509ad1c9d6d6eb92bc802
Tianxi Ku <kutianxi@outlook.com>
2024-04-14 23:44:57 +0800

    fix floaterm keybinding

--- nvim/lua/configs/floaterm.lua
+++ nvim/lua/configs/floaterm.lua
@@ -2,6 +2,7 @@ vim.keymap.set({ "n", "v" }, "<space>f", ":FloatermToggle<CR>", { noremap = true
 vim.keymap.set('t', "<leader>q", "<C-\\><C-n>:FloatermToggle<CR>", { noremap = true, silent = true })
 -- Kill all instances of floaterm on qall
 vim.keymap.set({ "n", "v" }, "WQ", ":FloatermKill!<CR>:wqall<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "WQ", "<C-\\><C-n>:FloatermKill!<CR>:wqall<CR>", { noremap = true, silent = true })
 
 -- Lazygit
 vim.cmd("command! Lazygit FloatermNew --width=1.0 --height=1.0 lazygit")
