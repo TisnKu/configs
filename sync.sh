93625d14aeac410891e959fabced519a890f74fd
Tianxi Ku <kutianxi@outlook.com>
2025-01-25 16:22:06 +0800

    update yml to toml

--- sync.sh
+++ sync.sh
@@ -10,19 +10,19 @@ ln -s $BASEDIR/nvim/ideavimrc ~/.ideavimrc
 # init nvim
 ln -s $BASEDIR/nvim ~/.config/
 
-# add import alacritty statement to ~/.config/alacritty/alacritty.yml
-# example: import: \n - ~/configs/alacritty/alacritty.yml
-# if alacritty folder does not exist or yml do not exist, create it
+# add import alacritty statement to ~/.config/alacritty/alacritty.toml
+# example: import: \n - ~/configs/alacritty/alacritty.toml
+# if alacritty folder does not exist or toml do not exist, create it
 if [ ! -d ~/.config/alacritty ]; then
     mkdir ~/.config/alacritty
 fi
-if [ ! -f ~/.config/alacritty/alacritty.yml ]; then
-    touch ~/.config/alacritty/alacritty.yml
+if [ ! -f ~/.config/alacritty/alacritty.toml ]; then
+    touch ~/.config/alacritty/alacritty.toml
 fi
 
-if ! grep -q "configs" ~/.config/alacritty/alacritty.yml; then
-    echo "import:" >> ~/.config/alacritty/alacritty.yml
-    echo "  - ~/configs/alacritty/alacritty.yml" >> ~/.config/alacritty/alacritty.yml
+if ! grep -q "configs" ~/.config/alacritty/alacritty.toml; then
+    echo "import:" >> ~/.config/alacritty/alacritty.toml
+    echo "  - ~/configs/alacritty/alacritty.toml" >> ~/.config/alacritty/alacritty.toml
 fi
 
 
