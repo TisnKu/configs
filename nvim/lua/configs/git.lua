1ccac2bb0a821fe39506823760c955665b60af24
Tianxi Ku <kutianxi@outlook.com>
2025-07-22 09:26:31 +0800

    'Autocommit'

--- nvim/lua/configs/git.lua
+++ nvim/lua/configs/git.lua
@@ -1,6 +1,21 @@
 -- If the wording directory is like **/notes, auto add files to git, commit and pull rebase and push every 30s
 
 local timer = nil
+
+local function run_git_sync(callback)
+  local cmd, args
+  if vim.loop.os_uname().sysname == "Windows_NT" then
+    cmd = "cmd"
+    args = { "/C", "git add . && git commit -m \"Auto commit\" && git pull --rebase && git push" }
+  else
+    cmd = "sh"
+    args = { "-c", "git add . && git commit -m 'Auto commit' && git pull --rebase && git push" }
+  end
+  vim.system(
+    vim.list_extend({ cmd }, args),
+    { text = true }, callback)
+end
+
 local function auto_git_sync()
   if timer then
     timer:stop()
@@ -15,11 +30,9 @@ local function auto_git_sync()
       if vim.fn.mode() ~= "n" then
         return
       end
-      vim.system(
-        { "pwsh", "-NoProfile", "-Command", "git add .; git commit -m 'Auto commit'; git pull --rebase; git push" },
-        { text = true }, function(_)
-          print("Git sync completed.")
-        end)
+      run_git_sync(function(_)
+        print("Git sync completed.")
+      end)
     end)
     timer:stop()
     timer:close()
